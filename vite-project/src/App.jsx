152bfd7470b4aa9d81f949ec7951b88b73040fa2
Nicolae <106536189+nicolae-co@users.noreply.github.com>
2023-02-01 14:19:22 +0200

    hold dices

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,22 +7,41 @@ function App() {
 
   const [dice, setDice] = useState(allNewDice())
 
+
+  function generateNewDie() {
+    const randomNumber = Math.ceil(Math.random() * 6) 
+    return{
+      value: randomNumber,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function allNewDice() {  
     const newDice = []
 
     for (let i = 0; i < 10; i++){
-      const randomNumber = Math.ceil(Math.random() * 6) 
-      newDice.push({
-        value: randomNumber,
-        isHeld: true,
-        id: nanoid()
-      })     
+
+      newDice.push(generateNewDie())     
     }
     return newDice
   }
 
+
   function rollDice() {
-    setDice(allNewDice())
+    setDice(prevState=> prevState.map(die => {
+      return die.isHeld ?
+      die: 
+      generateNewDie()
+    }))
+  }
+
+  function holdDice(id){
+    setDice(prevState =>{
+      return prevState.map((die)=>{
+        return die.id === id ? {...die, isHeld: !die.isHeld} : die
+      })
+    })
   }
   
   const diceElements = dice.map(item => (
@@ -30,6 +49,7 @@ function App() {
       number={item.value}
       key={item.id}
       isHeld={item.isHeld}
+      holdDice={() => holdDice(item.id)}
     />
   ))
 
@@ -39,6 +59,11 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the 
+                  same. Click each die to freeze it at its current value 
+                  between rolls.
+            </p>
       <div className='dice-container'>
         {diceElements}
       </div>
--- vite-project/src/components/Die.jsx
+++ vite-project/src/components/Die.jsx
@@ -1,6 +1,9 @@
 function Die(props) {
     return (
-        <div className={props.isHeld ? "die-face bg-green" : "die-face"}>
+        <div 
+            className={props.isHeld ? "die-face bg-green" : "die-face"}
+            onClick={props.holdDice}    
+        >
             <h2 className="die-num">{props.number}</h2>
         </div>
     )
